3adc04123491c36bca9eceb6258824457032e466
fran <francesco.beccaria@gmail.com>
2020-12-19 18:34:36 +0100

    2020 Day 3

---
+++ 2020/day-3/part-2/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"advent-of-code/utils"
+)
+
+var (
+	year       = 2020
+	day        = 3
+	input      = utils.GetInput(year, day)
+	inputArray = strings.Fields(input)
+)
+
+func main() {
+	fmt.Println(countTrees(inputArray, 1, 1) * countTrees(inputArray, 3, 1) * countTrees(inputArray, 5, 1) * countTrees(inputArray, 7, 1) * countTrees(inputArray, 1, 2))
+}
+
+func countTrees(a []string, x int, y int) int {
+	lineLength := len((a)[0])
+	trees := 0
+	posHor := 0
+	posVer := 0
+
+	for _, s := range a {
+		if posVer%y == 0 {
+			if string(s[posHor]) == "#" {
+				trees++
+			}
+			posHor = (posHor + x) % lineLength
+		}
+		posVer++
+	}
+
+	return trees
+}
